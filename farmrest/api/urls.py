d15d283f4b62dec0fa52c4406e49882eb4d019a8
mjgreeno <mjgreeno@gmail.com>
2018-01-31 02:35:23 -0600

    adding more endpoints

--- farmrest/api/urls.py
+++ farmrest/api/urls.py
@@ -2,10 +2,19 @@ from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from .views import TotalGpsMessagesView
 from .views import TotalUniqueMessages
+from .views import TimeBetweenTimeStamps
+from .views import AverageMessages
+from .views import MostCanMessages
+
 
 urlpatterns = {
     url(r'^totalgps/$', TotalGpsMessagesView.as_view(), name="Total GPS Messages"),
     url(r'^totaluniquecan/$', TotalUniqueMessages.as_view(), name="Total Unique CAN Messages"),
+    url(r'^timebetweendates/$', TimeBetweenTimeStamps.as_view(), name="Total Time of Dataset"),
+    url(r'^avgmessages/$', AverageMessages.as_view(), name="Average CAN and GPS Messages per second"),
+    url(r'^mostcanmessages/$', MostCanMessages.as_view(), name="Most CAN messages from timestamp"),
 }
 
 urlpatterns = format_suffix_patterns(urlpatterns)
+
+
--- farmrest/api/views.py
+++ farmrest/api/views.py
@@ -1,10 +1,9 @@
 from rest_framework import generics, permissions
 from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
-from django.http import JsonResponse
-from .serializers import FarmDataSerializer
 from .models import FarmData
-# Create your views here.
+from django.db import connection
+from django.http import HttpResponse
 
 
 class TotalGpsMessagesView(generics.ListAPIView):
@@ -17,10 +16,6 @@ class TotalGpsMessagesView(generics.ListAPIView):
         content = {'gps_message_count': gps_count, 'can_message_count': can_count}
         return Response(content)
 
-        #queryset = FarmData.objects.exclude(gps_id=0).count()
-    #queryset = FarmData.objects.raw('SELECT COUNT(id) from api_farmdata where gps_id != ""')
-    #serializer_class = FarmDataSerializer
-
 
 class TotalUniqueMessages(generics.ListAPIView):
     """API Get to acquire total Unique messages namely CAN messages"""
@@ -31,3 +26,52 @@ class TotalUniqueMessages(generics.ListAPIView):
         content = {'unique_can_messages': unique_messages}
         return Response(content)
 
+
+class TimeBetweenTimeStamps(generics.ListAPIView):
+    """API Get to acquire time between dates and date time operations"""
+    renderer_classes = (JSONRenderer,)
+
+    def get(self, request, format=None):
+
+        cursor = connection.cursor()
+        cursor.execute(
+            """SELECT (strftime('%s',max(ts)) - strftime('%s', min(ts))) As seconds FROM api_farmdata""")
+        rows = cursor.fetchall()
+        result = []
+        keys = ('seconds',)
+        for row in rows:
+            result.append(dict(zip(keys, row)))
+        json_data = json.dumps(result)
+        return HttpResponse(json_data, content_type="application/json")
+
+
+class AverageMessages(generics.ListAPIView):
+    """API Get to acquire Average CAN messages per second of run time and per gps message"""
+    renderer_classes = (JSONRenderer,)
+
+    def get(self, request, format=None):
+
+        cursor = connection.cursor()
+        cursor.execute(
+            """SELECT (strftime('%s',max(ts)) - strftime('%s', min(ts))) / cast(count(message_id) as float) AS test FROM api_farmdata WHERE message_id != ''""")
+        rows = cursor.fetchone()
+        content = {'avg_can_messages': rows}
+        return Response(content)
+
+
+class MostCanMessages(generics.ListAPIView):
+    """The first ts (timestamp) that contains the most CAN messages"""
+    renderer_classes = (JSONRenderer,)
+
+    def get(self, request, format=None):
+        cursor = connection.cursor()
+        cursor.execute(
+            """select ts, count(*) seconddata  from ( 
+               select message_id, ts, count(*) seconddata FROM api_farmdata WHERE message_id != "" group by message_id, ts order by seconddata desc
+               ) as a 
+               group by ts
+               order by seconddata desc
+              limit 1""")
+        rows = cursor.fetchone()
+        content = {'most_can_messages': rows}
+        return Response(content)
